09c8c2b74f034e87175e42613c623ee302631521
renan <renan.nic28@gmail.com>
2024-12-04 10:30:16 -0300

    feat: get full items in cart

--- src/main/java/com/renigomes/api_livraria/cart/service/CartService.java
+++ src/main/java/com/renigomes/api_livraria/cart/service/CartService.java
@@ -1,13 +1,10 @@
 package com.renigomes.api_livraria.cart.service;
 
-import com.renigomes.api_livraria.cart.model.Cart;
 import com.renigomes.api_livraria.cart.repository.CartRepository;
 import com.renigomes.api_livraria.item_book.model.ItemBook;
 import com.renigomes.api_livraria.item_book.service.ItemBookService;
-import com.renigomes.api_livraria.user.model.User;
 import com.renigomes.api_livraria.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
