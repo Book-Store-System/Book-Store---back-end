513e40d0f9d5daebb42d8d9d0b76d99daac22461
renigoms <renan.nic28@gmail.com>
2025-05-19 21:35:17 -0300

    doc: class diagran update

--- src/main/resources/db/migration/V20250518151139__rename_columns.sql
+++ src/main/resources/db/migration/V20250518151139__rename_columns.sql
@@ -1,12 +1,12 @@
 -- author table
 ALTER TABLE book ALTER COLUMN author TYPE INT USING author::integer;
 
-ALTER TABLE book RENAME author TO author_id;
+ALTER TABLE book RENAME COLUMN author TO author_id;
 
 ALTER TABLE book ADD CONSTRAINT fk_book_author FOREIGN KEY (author_id) REFERENCES author (id);
 
 -- category table
-ALTER TABLE book RENAME genre TO category_id;
+ALTER TABLE book RENAME COLUMN genre TO category_id;
 
 ALTER TABLE book ALTER COLUMN category_id TYPE INT USING category_id::integer;
 
@@ -16,6 +16,6 @@ ALTER TABLE book ADD CONSTRAINT fk_book_category FOREIGN KEY (category_id) REFER
 -- publisher table
 ALTER TABLE book ALTER COLUMN publisher TYPE INT USING publisher::integer;
 
-ALTER TABLE book RENAME publisher TO publisher_id;
+ALTER TABLE book RENAME COLUMN publisher TO publisher_id;
 
 ALTER TABLE book ADD CONSTRAINT fk_book_publisher FOREIGN KEY (publisher_id) REFERENCES publisher (id);
